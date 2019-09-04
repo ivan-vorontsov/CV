2f413c71270e807ce95776fd5a438b7bb8d74a0c
ivan_vorontsov_2019 <ivan.vorontsov.2019@gmail.com>
2019-09-05 02:45:01 +0300

    Active menu item

--- VIS/CV/src/app/models/menu.model.ts
+++ VIS/CV/src/app/models/menu.model.ts
@@ -2,5 +2,6 @@ import { MenuItem } from "./menu.item.model";
 
 export class Menu {
     constructor(public id?: number,
-        public items?: MenuItem[]) { }
+        public items?: MenuItem[],
+        public activeItem?: MenuItem) { }
 }
