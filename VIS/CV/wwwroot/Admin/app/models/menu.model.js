aa92575def502ede98036b939169f73618abb150
IvanVorontsov <ivan.labelplatform@gmail.com>
2019-09-05 13:28:57 +0300

    Commit all

--- VIS/CV/wwwroot/Admin/app/models/menu.model.js
+++
@@ -1,7 +0,0 @@
-export class Menu {
-    constructor(id, items) {
-        this.id = id;
-        this.items = items;
-    }
-}
-//# sourceMappingURL=menu.model.js.map
--- VIS/CV/wwwroot/Admin/polyfills-es2015.js
+++ VIS/CV/wwwroot/Admin/polyfills-es2015.js
@@ -6370,8 +6370,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! G:\dev\CV\VIS\CV\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! G:\dev\CV\VIS\CV\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! D:\CV\VIS\CV\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! D:\CV\VIS\CV\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
